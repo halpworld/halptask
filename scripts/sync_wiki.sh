3487f60c79621a1ee6b8a73f141a0e0cd5f0f7b8
Kenth Fagerlund <kenth@muthur.net>
2026-08-13 12:34:45 +0100

    fix(scripts): use HTTPS URL in sync_wiki.sh

--- scripts/sync_wiki.sh
+++ scripts/sync_wiki.sh
@@ -6,7 +6,7 @@ set -e
 SOURCE_DIR="$(pwd)"
 WIKI_DIR="${SOURCE_DIR}/wiki"
 TEMP_DIR=$(mktemp -d)
-REMOTE_REPO="git@github.com:arkalon76/halptask.wiki.git"
+REMOTE_REPO="https://github.com/arkalon76/halptask.wiki.git"
 
 echo "🚀 Syncing HalpTask documentation in '${WIKI_DIR}' to GitHub Wiki (${REMOTE_REPO})..."
 
